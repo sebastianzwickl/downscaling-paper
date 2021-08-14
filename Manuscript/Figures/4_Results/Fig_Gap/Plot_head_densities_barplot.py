002a702e54fe489fdaae0710d018ae7eac4c52a1
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2021-08-14 11:33:44 +0200

    Close #15 by updating heat density comparison figure

--- Manuscript/Figures/4_Results/Fig_Gap/Plot_head_densities_barplot.py
+++ Manuscript/Figures/4_Results/Fig_Gap/Plot_head_densities_barplot.py
@@ -1,6 +1,7 @@
 import pyam as py
 import matplotlib.pyplot as plt
 import matplotlib.colors as c
+import numpy as np
 
 _c_list = ["#284E78", "#FF96AD"]
 _new_cm = c.ListedColormap(colors=_c_list, name="New cmap")
@@ -17,10 +18,21 @@ _fig = [fig_DT, fig_SC, fig_TF, fig_GD]
 sce = ["Directed Transition", "Societal Commitment", "Techno-Friendly", "Gradual Development"]
 a_tuple = zip(_fig, sce)
 data = py.IamDataFrame("Heat_density_algorithm2.xlsx")
+data.filter(variable="Heat density", inplace=True)
 
 
 for _tup in a_tuple:
     plot = data.filter(scenario=_tup[1], keep=True)
+    
+    val = plot.data["value"]
+    
+    for i, v in enumerate(val):
+        _tup[0].text(i, v+0.5, str(np.round(v,2)), color='black', ha="center", va="center", fontsize=6)
+        
+    
+    # for i, v in enumerate(height):
+    # ax.text(i+1, v+0.25, str(np.round(v-_red[i],2)), color='black', ha="center", va="center", fontsize=10)
+    
     bars = plot.plot.bar(ax=_tup[0], 
                   x="region", 
                   stacked=True, 
@@ -31,7 +43,7 @@ for _tup in a_tuple:
                   cmap=_new_cm,
                   bars_order=["Heat density", "Heat density gap (2050 and today)"],
                   alpha=1)
-    _tup[0].set_ylim([0,10.5])
+    # _tup[0].set_ylim([0,10.5])
     _tup[0].set_title(_tup[1], pad=0)
     _tup[0].set_xlabel("")
     _tup[0].set_ylabel("")
@@ -39,19 +51,19 @@ for _tup in a_tuple:
     _tup[0].tick_params(axis='y', which='major', pad=2)
     _tup[0].tick_params(axis='x', which='major', pad=2)
     _tup[0].set_xticklabels(labels=["South\nViennesse\nenviron\n(AT127)", "Vienna\n(AT130)", "Graz\n(AT221)", "Linz-Wels\n(AT312)", "Salzburg\n(AT323)", "Rheintal-\nBodensee\n(AT342)"])
-    if _tup[1] == "Techno-Friendly":
-        plt.rcParams['hatch.linewidth'] = 1
-        bars.patches[8].set_hatch("///")
-        bars.patches[8].set_edgecolor('#FFF5FD')
+    # if _tup[1] == "Techno-Friendly":
+    #     plt.rcParams['hatch.linewidth'] = 1
+    #     bars.patches[8].set_hatch("///")
+    #     bars.patches[8].set_edgecolor('#FFF5FD')
+    ymin, ymax = _tup[0].get_ylim()
+    _tup[0].set_ylim([ymin, ymax+1])
 
         
     
     
 handles, labels = _tup[0].get_legend_handles_labels()
-fig.legend(handles, labels, ncol=2, bbox_to_anchor=(0.5,0.855), loc="center")
-
-fig.suptitle("Heat density of centralized heat networks in Austrian sub-regions 2050\nin the four different decarbonization scenarios "+r"in $\frac{GWh}{km^2}$", y=0.99)
-plt.tight_layout(h_pad=0.5)
+fig.suptitle("Heat density of centralized heat networks in Austrian sub-regions 2050\nin the four different decarbonization scenarios "+r"in $\frac{GWh}{km^2}$", y=0.95)
+plt.tight_layout()
 fig.savefig("HD1.png", dpi=500)
 fig.savefig("HD1.eps", format="eps") 
 
